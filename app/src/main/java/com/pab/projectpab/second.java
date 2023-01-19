f9367ae3e9dd2e1b853810944df2cd10062bb0db
Dwi <sastikadwiapriyanti2@mhs.mdp.ac.id>
2023-01-19 22:27:57 +0700

    22.27

--- app/src/main/java/com/pab/projectpab/second.java
+++ app/src/main/java/com/pab/projectpab/second.java
@@ -1,6 +1,14 @@
 package com.pab.projectpab;
 
-import android.app.Activity;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class second extends Activity {
+import android.os.Bundle;
+
+public class Second extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
 }
